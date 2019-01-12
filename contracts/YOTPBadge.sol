c043114fdf33c39fe0ebd8332e006b7a9cc50636
Kim Persson <kimpersson88@gmail.com>
2019-01-12 21:49:57 +0100

    ADDS badge info with donation metadata

--- contracts/YOTPBadge.sol
+++ contracts/YOTPBadge.sol
@@ -7,13 +7,24 @@ import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 
 contract YOTPBadge is ERC721Full, ERC721Mintable, Ownable {
     uint maxSupply;
+    mapping(uint => BadgeInfo) public badgeInfos;
 
     constructor(uint _maxSupply)
         ERC721Full("YearOfThePigBadge", "YPB")
         public
     {
         require(_maxSupply > 0, "requires positive max supply");
+
         maxSupply = _maxSupply;
+    }
+
+    enum BadgeLevel { Bronze, Silver, Gold }
+
+    struct BadgeInfo {
+        uint amount;
+        string charityId;
+        BadgeLevel badgeLevel;
+        uint createdAt;
     }
 
     /**
@@ -37,7 +48,12 @@ contract YOTPBadge is ERC721Full, ERC721Mintable, Ownable {
     * @param _to address for the receiver of bage
     * @param _tokenURI IPFS hash for badge image
     */
-    function mintTo(address _to, string memory _tokenURI)
+    function mintTo(
+        address _to,
+        string memory _tokenURI,
+        uint _amount,
+        string memory _charityId
+    )
         public
         onlyOwner
         lessThanMaxSupply
@@ -46,10 +62,60 @@ contract YOTPBadge is ERC721Full, ERC721Mintable, Ownable {
         uint256 newTokenId = _getNextTokenId();
         _mint(_to, newTokenId);
         _setTokenURI(newTokenId, _tokenURI);
+        _setBadgeInfo(newTokenId, _amount, _charityId);
 
         emit LogMinted(newTokenId, _tokenURI);
 
         return newTokenId;
+    }
+
+    /**
+    * @dev returns the donation badge level for certain donation amount
+    * @param amount uint charity donation in wei
+    */
+    function _getBadgeLevel(uint amount)
+        pure
+        private
+        returns (BadgeLevel)
+    {
+        // 0.5 eth for gold badge level donation
+        if (amount >= 500000000000000000) {
+            return BadgeLevel.Gold;
+        // 0.1 eth for silver badge level donation
+        } else if (amount >= 100000000000000000) {
+            return BadgeLevel.Silver;
+        }
+
+        return BadgeLevel.Bronze;
+    }
+
+
+
+    /**
+    * @dev sets metadata info for a bage
+    * @param tokenId uint id of token created previously
+    * @param amount uint amount in wei that was donated to charity
+    * @param charityId string id of charity org to which donation was made
+    */
+    function _setBadgeInfo(
+        uint tokenId,
+        uint amount,
+        string memory charityId
+    )
+        private
+    {
+        require(_exists(tokenId));
+
+        BadgeLevel badgeLevel = _getBadgeLevel(amount);
+
+        BadgeInfo memory badgeInfo = BadgeInfo(
+            amount,
+            charityId,
+            badgeLevel,
+            now
+        );
+
+        badgeInfos[tokenId] = badgeInfo;
     }
 
     /**
--- test/YOTPBadge.js
+++ test/YOTPBadge.js
@@ -5,6 +5,9 @@ import truffleAssert from "truffle-assertions";
 
 const MAX_SUPPLY = 2;
 
+const SILVER_DONATION_AMOUNT = "100000000000000000";
+const GOLD_DONATION_AMOUNT = "500000000000000000";
+
 contract("YOTPBadge", accounts => {
   let badge;
   beforeEach(async () => {
@@ -25,9 +28,15 @@ contract("YOTPBadge", accounts => {
 
   describe("minting", () => {
     it("allows owner to mint tokens", async () => {
-      const result = await badge.mintTo(nonOwner, "ipfshash", {
-        from: owner
-      });
+      const result = await badge.mintTo(
+        nonOwner,
+        "ipfshash",
+        SILVER_DONATION_AMOUNT,
+        "DogeCharity",
+        {
+          from: owner
+        }
+      );
 
       truffleAssert.eventEmitted(
         result,
@@ -39,7 +48,7 @@ contract("YOTPBadge", accounts => {
 
     it("does not allow non-owner to mint tokens", async () => {
       await truffleAssert.fails(
-        badge.mintTo(owner, "ipfshash", {
+        badge.mintTo(owner, "ipfshash", SILVER_DONATION_AMOUNT, "DogeCharity", {
           from: nonOwner
         }),
         truffleAssert.ErrorType.REVERT
@@ -48,24 +57,83 @@ contract("YOTPBadge", accounts => {
 
     it(`only allows maxSupply to be minted`, async () => {
       await truffleAssert.passes(
-        badge.mintTo(nonOwner, "ipfshash", {
-          from: owner
-        })
+        badge.mintTo(
+          nonOwner,
+          "ipfshash",
+          SILVER_DONATION_AMOUNT,
+          "DogeCharity",
+          {
+            from: owner
+          }
+        )
       );
 
       await truffleAssert.passes(
-        badge.mintTo(nonOwner, "ipfshash", {
-          from: owner
-        })
+        badge.mintTo(
+          nonOwner,
+          "ipfshash",
+          SILVER_DONATION_AMOUNT,
+          "DogeCharity",
+          {
+            from: owner
+          }
+        )
       );
 
       await truffleAssert.fails(
-        badge.mintTo(nonOwner, "ipfshash", {
-          from: owner
-        }),
+        badge.mintTo(
+          nonOwner,
+          "ipfshash",
+          SILVER_DONATION_AMOUNT,
+          "DogeCharity",
+          {
+            from: owner
+          }
+        ),
         truffleAssert.ErrorType.REVERT,
         "max supply reached"
       );
+    });
+
+    it("sets badge level gold for 0.5 eth donations", async () => {
+      await badge.mintTo(
+        nonOwner,
+        "ipfshash",
+        GOLD_DONATION_AMOUNT,
+        "DogeCharity",
+        { from: owner }
+      );
+
+      const results = await badge.badgeInfos.call(1);
+      assert.equal(results.amount.toString(), GOLD_DONATION_AMOUNT);
+      assert.equal(results.charityId, "DogeCharity");
+      assert.equal(results.badgeLevel.toNumber(), 2);
+    });
+
+    it("sets badge level silver for 0.1 eth donations", async () => {
+      await badge.mintTo(
+        nonOwner,
+        "ipfshash",
+        SILVER_DONATION_AMOUNT,
+        "DogeCharity",
+        { from: owner }
+      );
+
+      const results = await badge.badgeInfos.call(1);
+      assert.equal(results.amount.toString(), SILVER_DONATION_AMOUNT);
+      assert.equal(results.charityId, "DogeCharity");
+      assert.equal(results.badgeLevel.toNumber(), 1);
+    });
+
+    it("sets badge level bronze for any donation", async () => {
+      await badge.mintTo(nonOwner, "ipfshash", 1, "DogeCharity", {
+        from: owner
+      });
+
+      const results = await badge.badgeInfos.call(1);
+      assert.equal(results.amount.toString(), 1);
+      assert.equal(results.charityId, "DogeCharity");
+      assert.equal(results.badgeLevel.toNumber(), 0);
     });
   });
 });
