5815daaa32f7040321a20f1df07ddf84cdbbf7e3
Kim Persson <kimpersson88@gmail.com>
2019-02-02 00:34:23 +0100

    ADDS dev chain to truffle config

--- truffle.js
+++ truffle.js
@@ -5,5 +5,12 @@ require("babel-polyfill");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts")
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 9545,
+      network_id: "*" // Match any network id
+    }
+  }
 };
