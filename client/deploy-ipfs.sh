69664a31734da0822b3735062b8e21252742bcb4
Kim Persson <kimpersson88@gmail.com>
2019-02-05 23:50:11 +0100

    ADDS full ipfs output for uploads in deploy-to-ipfs script

--- client/deploy-ipfs.sh
+++ client/deploy-ipfs.sh
@@ -2,7 +2,9 @@ rm -rf build
 yarn build
 
 echo "Deploying to IPFS..."
-hash_row=$(ipfs add -r build | grep build | tail -1)
+output=$(ipfs add -r build )
+echo "${output}"
+hash_row=$(echo "${output}"| grep build | tail -1)
 hash_row_arr=($hash_row)
 url="https://cloudflare-ipfs.com/ipfs/${hash_row_arr[1]}"
 echo "Deploy successful. Content available at ${url} (copied to clipboard)"
